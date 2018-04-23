0f583c09f30afc802c621a2742e1b102f9688506
Daniel Nussbaum <daniel.e.nussbaum@gmail.com>
2018-04-23 16:27:05 -0700

    showcase colors and stock photos/copy

--- myappmba-landing/index.php
+++ myappmba-landing/index.php
@@ -175,21 +175,21 @@
           <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/bg-showcase-1.jpg');"></div>
           <div class="col-lg-6 order-lg-1 my-auto showcase-text">
             <h2>Essay Drafts, Wrangled</h2>
-            <p class="lead mb-0">When you use a theme created by Start Bootstrap, you know that the theme will look great on any device, whether it's a phone, tablet, or desktop the page will behave responsively!</p>
+            <p class="lead mb-0">Writing your application essays can be the hardest part of the application process. myapp.MBA allows you to focus on your story—we take care of the rest with a suite of tools to make managing drafts, versions, and prompts a breeze.</p>
           </div>
         </div>
         <div class="row  no-gutters">
           <div class="col-lg-6 text-white showcase-img" style="background-image: url('img/bg-showcase-2.jpg');"></div>
           <div class="col-lg-6 my-auto showcase-text">
             <h2>Deadlines, Managed</h2>
-            <p class="lead mb-0">Newly improved, and full of great utility classes, Bootstrap 4 is leading the way in mobile responsive web development! All of the themes on Start Bootstrap are now using Bootstrap 4!</p>
+            <p class="lead mb-0">Every school has different requirements and timelines. Managing your deadlines doesn't have to be a pain. myapp.MBA keeps track of it all so you can focus on the more important things.</p>
           </div>
         </div>
         <div class="row  no-gutters">
           <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/bg-showcase-3.jpg');"></div>
           <div class="col-lg-6 order-lg-1 my-auto showcase-text">
             <h2>Advice, On-Demand</h2>
-            <p class="lead mb-0">Landing Page is just HTML and CSS with a splash of SCSS for users who demand some deeper customization options. Out of the box, just add your content and images, and your new landing page will be ready to go!</p>
+            <p class="lead mb-0">If you get stuck on an essay or have a question, we have specialists who can review your application materials for a fraction of what an MBA consultant would cost.</p>
           </div>
         </div>
       </div>

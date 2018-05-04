d21894ff03e3e7dc56f24d4df3f27ab3410876d3
Daniel Nussbaum <daniel.e.nussbaum@gmail.com>
2018-05-04 18:14:01 -0500

    updated header and enabled authentication for users endpoints

--- angular/dist/demo/default/src/app/_services/users.service.ts
+++ angular/dist/demo/default/src/app/_services/users.service.ts
@@ -1,19 +1,35 @@
-import { Injectable } from '@angular/core';
-import { Http, Response, Headers } from '@angular/http';
-import { URL, user} from './url-infos'
+import {Injectable} from '@angular/core';
+import {Http, Response, Headers, RequestOptions} from '@angular/http';
+import {URL, user} from './url-infos'
 
 let header = new Headers({'Content-Type': 'application/json'});
+
 @Injectable()
 export class UsersService {
 
- constructor(private http: Http) { }
+    constructor(private http: Http) {
+    }
 
- getUser() {
-    return this.http.get(URL+"/mba/users/"+user).map((response: Response) => response.json());
- }
+    getUser() {
+        return this.http.get(URL + "/mba/users/" + user, this.jwt(1)).map((response: Response) => response.json());
+    }
 
- modifyUser(data) {
-    this.http.put(URL+"/mba/users/"+user+"/", data, {headers: header});
- }
+    modifyUser(data) {
+        this.http.put(URL + "/mba/users/" + user + "/", data, this.jwt(1));
+    }
+
+    private jwt(json = 0) {
+        // create authorization header with jwt token
+        let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        if (currentUser && currentUser.AUTH_TOKEN) {
+            let headers = new Headers;
+            if (json == 1) {
+                headers = new Headers({'Authorization': currentUser.AUTH_TOKEN, 'Content-Type': 'application/json'});
+            } else {
+                headers = new Headers({'Authorization': currentUser.AUTH_TOKEN});
+            }
+            return new RequestOptions({headers: headers});
+        }
+    }
 
 }
--- angular/dist/demo/default/src/app/theme/layouts/header-nav/header-nav.component.ts
+++ angular/dist/demo/default/src/app/theme/layouts/header-nav/header-nav.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit, ViewEncapsulation, AfterViewInit } from '@angular/core';
 import { Helpers } from '../../../helpers';
+import { UsersService } from '../../../_services/users.service';
+
 
 declare let mLayout: any;
 @Component({
     selector: "app-header-nav",
     templateUrl: "./header-nav.component.html",
     encapsulation: ViewEncapsulation.None,
+    providers: [UsersService]
 })
 export class HeaderNavComponent implements OnInit, AfterViewInit {
+    user = {};
 
-
-    constructor() {
-
+    constructor(private _users:UsersService) {
+        this._users.getUser().subscribe(d => this.user = d);
     }
     ngOnInit() {
 

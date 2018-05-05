8a0db746d29e8bfbfd2809768f73c7532b5fea38
Daniel Nussbaum <daniel.e.nussbaum@gmail.com>
2018-05-05 14:57:50 -0500

    delete school works properly now

--- angular/dist/demo/default/src/app/theme/pages/default/index/index.component.ts
+++ angular/dist/demo/default/src/app/theme/pages/default/index/index.component.ts
@@ -18,15 +18,22 @@ export class IndexComponent implements OnInit, AfterViewInit {
     chosenValue: string;
 
     constructor(private _script: ScriptLoaderService, private _schools:SchoolsService) {
-      this._schools.getSchools().subscribe(d => this.schools = d);
-      this._schools.getAllSchools().subscribe(d => this.allSchools = d);
+        this.getSchools();
+        this._schools.getAllSchools().subscribe(d => this.allSchools = d);
     }
 
-    deleteSchool(schoolName: string) {
+    getSchools() {
+        this._schools.getSchools().subscribe(d => this.schools = d);
+    }
+
+    deleteSchool(event, index, schoolName: string) {
+        event.stopPropagation();
+        this.schools.splice(index, 1);
         this._schools.userDeleteSchool(schoolName);
     }
+
     ngOnInit() {
-        this._schools.getSchools().subscribe(d => this.schools = d);
+        this.getSchools();
     }
 
     
@@ -39,17 +46,16 @@ export class IndexComponent implements OnInit, AfterViewInit {
     notEmpty() {
         return this.schools != undefined && this.allSchools != undefined;
     }
+
     addSchool() {
         for (var i = 0; i < this.allSchools.length; i++) {
             if (this.allSchools[i].name === this.chosenValue) {
                 this._schools.userAddSchool(this.allSchools[i].shortName).subscribe(
                     (response:Response) => {
-
+                        this.getSchools();
                     }, (error:Response) => {
-                        console.log(error);
                     }
                 );
-                this.schools.push(this.allSchools[i]);
                 break;
             }
         }

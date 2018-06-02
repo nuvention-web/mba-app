2fc06f68191fc709af932ee600833d3a25e3402b
crazystir <qingtongguo2019@u.northwestern.edu>
2018-06-01 22:39:41 -0500

    Add deadline dropdown and the delete modal. Make the home page show different layout to different device. Improve the login webpage

--- angular/dist/src/app/_services/url-infos.ts
+++ angular/dist/src/app/_services/url-infos.ts
@@ -2,7 +2,7 @@
 import {Headers, RequestOptions} from "@angular/http";
 
 export const URL = "https://portal.myapp.mba";
-//export const URL = "http://myappmba-199623.appspot.com";
+// The reset password must use this endpoint. Otherwise the server report 400. export const URL = "http://myappmba-199623.appspot.com";
 
 export function getCurrentUser() {
     let currentUser = JSON.parse(localStorage.getItem('currentUser'));
